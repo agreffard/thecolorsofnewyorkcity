785c95d145b1c736f73b2e5e84e45d98e4758372
Aurélien Greffard <aurelien.greffard@gmail.com>
2018-02-03 13:26:10 +0000

    Improved UI image detail position

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -5,7 +5,7 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  styleUrls: ['./settings.component.css'],
   animations: [
     trigger('visibilityChanged', [
       state('true', style({ opacity: 1 })),
